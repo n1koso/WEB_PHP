8bf919b10b2878994e0e77b6c2ce83878c193957
n1koso <99286814+n1koso@users.noreply.github.com>
2022-02-11 13:31:49 +0200

    first task

--- code/index.php
+++ code/index.php
@@ -1,2 +1,15 @@
 <?php
-echo phpinfo();
+
+echo "1.Доступ по ссылке";
+/* Imagine a lot of code here */
+
+$very_bad_unclear_name = "15 chicken wings";
+
+// Write your code here:
+
+$order = &$very_bad_unclear_name;
+$order .= " for me";
+
+// Don't change the line below
+
+echo "<br>Your order is: $very_bad_unclear_name.";
