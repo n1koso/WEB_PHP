33df8905209d5aac288ef0eb241dc36c722cbad9
n1koso <99286814+n1koso@users.noreply.github.com>
2022-02-22 12:26:00 +0200

    4 task

--- code/index.php
+++ code/index.php
@@ -29,4 +29,11 @@ $num_languages = 4;
 $months = 11;
 $days = $months * 16;
 $days_per_language = $days / $num_languages;
-echo $days_per_language;
+echo $days_per_language . "<br>";
+
+
+echo "<br>12. Степень<br>";
+
+echo 8**2;
+
+
